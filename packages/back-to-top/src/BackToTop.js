c23b0674cbfe0e479174fd050cb4ebf206dba835
Phil Parsons <p-m-p@users.noreply.github.com>
2025-03-13 08:28:08 +0000

    feat(back-to-top): add option to scroll with container

--- packages/back-to-top/src/BackToTop.js
+++ packages/back-to-top/src/BackToTop.js
@@ -10,18 +10,37 @@ import stylesheet from './style.css' with { type: 'css' }
  */
 export default class CopyToClipboard extends HTMLElement {
   #controller = null
+  #scrollPosition = 0
+  #activationPoint = 500
 
   static register(tagName = 'back-to-top') {
     customElements.define(tagName, this)
   }
 
   connectedCallback() {
-    const { buttonLabel = 'Scroll back to top' } = this.dataset
+    const {
+      buttonLabel = 'Scroll back to top',
+      scrollBehavior = 'auto',
+      scrollContainer,
+      threshold,
+    } = this.dataset
 
     const template = document.createElement('template')
     template.innerHTML = `<slot>
   <button part="button" type="button" aria-label="${buttonLabel}">
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m18 15-6-6-6 6"/></svg>
+    <slot name="icon">
+      <svg part="icon" xmlns="http://www.w3.org/2000/svg"
+        width="1em"
+        height="1em"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round">
+        <path d="m18 15-6-6-6 6"/>
+      </svg>
+    </slot>
   </button>
 </slot>`
 
@@ -29,13 +48,24 @@ export default class CopyToClipboard extends HTMLElement {
     shadow.appendChild(template.content.cloneNode(true))
     shadow.adoptedStyleSheets.push(stylesheet)
 
-    this.#controller = new AbortController()
-    this.#setState()
+    const activationPoint = parseInt(threshold, 10)
 
-    window.addEventListener(
+    if (!isNaN(activationPoint)) {
+      this.#activationPoint = activationPoint
+    }
+
+    let parent
+
+    if (scrollContainer && document.getElementById(scrollContainer)) {
+      parent = document.getElementById(scrollContainer)
+    }
+
+    this.#setState(parent ? parent.scrollTop : window.scrollY)
+    this.#controller = new AbortController()
+    ;(parent ?? window).addEventListener(
       'scroll',
       () => {
-        this.#setState()
+        this.#setState(parent ? parent.scrollTop : window.scrollY)
       },
       {
         signal: this.#controller.signal,
@@ -43,7 +73,10 @@ export default class CopyToClipboard extends HTMLElement {
     )
 
     shadow.querySelector('slot').addEventListener('click', () => {
-      window.scrollTo({ top: 0, behavior: 'auto' })
+      ;(parent ?? window).scrollTo({
+        top: 0,
+        behavior: scrollBehavior,
+      })
     })
   }
 
@@ -51,11 +84,19 @@ export default class CopyToClipboard extends HTMLElement {
     this.#controller?.abort()
   }
 
-  #setState() {
-    if (window.scrollY > 500) {
+  /**
+   * @param {number} scrollPosition
+   */
+  #setState(scrollPosition) {
+    if (
+      scrollPosition > this.#activationPoint &&
+      scrollPosition < this.#scrollPosition
+    ) {
       this.dataset.state = 'active'
     } else {
       this.dataset.state = 'inactive'
     }
+
+    this.#scrollPosition = scrollPosition
   }
 }
--- stories/BackToTop/BackToTop.stories.js
+++ stories/BackToTop/BackToTop.stories.js
@@ -14,3 +14,15 @@ export const Default = {
   render: () =>
     html`<div class="page"><test-back-to-top></test-back-to-top></div>`,
 }
+
+export const Container = {
+  render: () => html`
+    <div class="container-wrapper">
+      <div class="container" id="scroller"><div class="page"></div></div>
+      <test-back-to-top
+        class="contained-btt"
+        data-scroll-behavior="smooth"
+        data-scroll-container="scroller"></test-back-to-top>
+    </div>
+  `,
+}
