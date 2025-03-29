ff24983eb311a1a8aca0a9e590d21ba1907544f5
Phil Parsons <p-m-p@users.noreply.github.com>
2025-03-29 11:11:09 +0000

    feat(theme-switch): add some initial toggle styles

--- packages/theme-switch/src/ThemeSwitch.js
+++ packages/theme-switch/src/ThemeSwitch.js
@@ -18,17 +18,22 @@ export default class ThemeSwitch extends HTMLElement {
       mode = window.matchMedia('(prefers-color-scheme: dark)').matches
         ? 'dark'
         : 'light'
+      this.dataset.mode = mode
     }
 
     const template = document.createElement('template')
     template.innerHTML = `<fieldset>
-  <label>
+  <label id="light">
     <input name="theme" type="radio" value="light" ${mode === 'light' ? 'checked ' : ''}/>
-    <slot name="light-label">Light</slot>
+    <slot name="light-label">
+      <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="4"/><path d="M12 2v2"/><path d="M12 20v2"/><path d="m4.93 4.93 1.41 1.41"/><path d="m17.66 17.66 1.41 1.41"/><path d="M2 12h2"/><path d="M20 12h2"/><path d="m6.34 17.66-1.41 1.41"/><path d="m19.07 4.93-1.41 1.41"/></svg>
+    </slot>
   </label>
-  <label>
+  <label id="dark">
     <input name="theme" type="radio" value="dark" ${mode === 'dark' ? 'checked ' : ''}/>
-    <slot name="dark-label">Dark</slot>
+    <slot name="dark-label">
+        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z"/></svg>
+    </slot>
   </label>
 </fieldset>
 `
@@ -39,13 +44,17 @@ export default class ThemeSwitch extends HTMLElement {
 
     shadow.addEventListener('change', (ev) => {
       const { strategy = 'class' } = this.dataset
+      // @ts-ignore
       const isDark = ev.target.value === 'dark'
+      const mode = isDark ? 'dark' : 'light'
 
       if (strategy === 'class') {
         document.documentElement.classList.toggle('dark', isDark)
       } else {
-        document.documentElement.dataset.theme = isDark ? 'dark' : 'light'
+        document.documentElement.dataset.theme = mode
       }
+
+      this.dataset.mode = mode
     })
   }
 }
